33e1493a0611fcaf5eae55fc13c5c695eefceee2
Lars Isferding <lars.isf@gmx.de>
2025-02-25 21:34:53 +0100

    Update entrypoint.sh

--- ts3server-box86/entrypoint.sh
+++ ts3server-box86/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "🔄 Starte TeamSpeak 3 Server (Version: $(cat /app/CHANGELOG | head -n1))"
+cd /app
+echo "TeamSpeak 3 Server startet..."
 exec box64 ./ts3server_minimal_runscript.sh
+
+chmod +x entrypoint.sh
